5175d806c624020393d6dd92e5d00d11226dd980
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2019-11-12 09:53:14 +0800

    Add project files.

---
+++ Dengue/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dengue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/prevention")]
+        public ActionResult Prevention()
+        {
+            return View();
+        }
+
+        [Route("/how")]
+        public ActionResult How()
+        {
+            return View();
+        }
+
+        [Route("/risk")]
+        public ActionResult Risk()
+        {
+            return View();
+        }
+    }
+}
