aec53b0fd11b5e684d56171bbbe77548b02b2e85
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-01-08 12:18:35 +0800

    hi

--- Dengue/Controllers/HomeController.cs
+++ Dengue/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
 using Microsoft.AspNetCore.Mvc;
+using Dengue.Models;
 
 namespace Dengue.Controllers
 {
@@ -11,6 +14,17 @@ namespace Dengue.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SignUp(EmailRecord emailRecord)
+        {
+            var firebaseClient = new FirebaseClient("https://denguelive-47836.firebaseio.com");
+            var result = await firebaseClient
+              .Child(emailRecord.Location)
+              .PostAsync(emailRecord);
+            TempData["msg"] = "<script>alert('Sign up successful');</script>";
+            return RedirectToAction("Index");
         }
 
         [Route("/prevention")]
@@ -27,6 +41,12 @@ namespace Dengue.Controllers
 
         [Route("/risk")]
         public ActionResult Risk()
+        {
+            return View();
+        }
+
+        [Route("/dashboard")]
+        public ActionResult Dashboard()
         {
             return View();
         }
---
+++ Dengue/Models/EmailRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dengue.Models
+{
+    public class EmailRecord
+    {
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
