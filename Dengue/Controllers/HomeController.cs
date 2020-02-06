8e82fced036419312ab88a5b74d84df11468ceb2
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-02-06 12:33:23 +0800

    init

--- Dengue/Controllers/HomeController.cs
+++ Dengue/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Firebase.Database;
 using Firebase.Database.Query;
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Mvc;
 using Dengue.Models;
+using Grpc.Auth;
 
 namespace Dengue.Controllers
 {
+
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -21,6 +24,7 @@ namespace Dengue.Controllers
         {
             var firebaseClient = new FirebaseClient("https://denguelive-47836.firebaseio.com");
             var result = await firebaseClient
+              .Child("Emails")
               .Child(emailRecord.Location)
               .PostAsync(emailRecord);
             TempData["msg"] = "<script>alert('Sign up successful');</script>";
@@ -46,8 +50,22 @@ namespace Dengue.Controllers
         }
 
         [Route("/dashboard")]
-        public ActionResult Dashboard()
+        public async Task<ActionResult> Dashboard()
         {
+            ////var auth = "nwzXF5q91brr8gvq426wr7qq6M5ocj26C36HOtaU"; // your app secret
+            //var firebaseClient = new FirebaseClient(
+            //  "https://denguelive-47836.firebaseio.com/");
+            //  //,
+            //  //new FirebaseOptions
+            //  //{
+            //  //    AuthTokenAsyncFactory = () => Task.FromResult(auth)
+            //  //});
+
+            //var result = await firebaseClient.Child("cases").OnceAsync<>();
+            //foreach (var location in result)
+            //{
+
+            //}
             return View();
         }
     }
---
+++ Dengue/Models/LocationResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Dengue.Models
+{
+    public class LocationResult
+    {
+        [JsonProperty(PropertyName = "location")]
+        public string Location { get; set; }
+        [JsonProperty(PropertyName = "probability 0-10")]
+        public string probability1 { get; set; }
+        [JsonProperty(PropertyName = "probability 11-21")]
+        public string probability2 { get; set; }
+        [JsonProperty(PropertyName = "probability 21-30")]
+        public string probability3 { get; set; }
+        [JsonProperty(PropertyName = "probability 31-40")]
+        public string probability4 { get; set; }
+        [JsonProperty(PropertyName = "probability 41-50")]
+        public string probability5 { get; set; }
+        [JsonProperty(PropertyName = "probability >51")]
+        public string probability6 { get; set; }
+    }
+}
